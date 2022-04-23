df211a693ab4ba12d9b932a492c5b054d1de0cc7
ryan273168 <ryan273168@gmail.com>
2022-04-23 15:26:09 +0800

    1234

---
+++ 20220423/20220423.py
@@ -0,0 +1,35 @@
+import turtle
+
+turtle.pensize(5)
+turtle.pencolor("black")
+turtle.fillcolor("orange")
+turtle.begin_fill()
+for i in range(5):
+    turtle.forward(200)
+    turtle.right(144)
+turtle.end_fill()
+turtle.done()
+# turtle.color("blue")
+# turtle.shape("turtle")
+
+# turtle.penup()
+# for step in range(5, 61, 2):
+#     turtle.stamp()
+#     turtle.forward(step)
+#     turtle.right(25)
+#     print(step)
+#turtle.done()
+# for i in range(4):
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.done()
+# for i in range(2,10,2):
+#     print(i)
