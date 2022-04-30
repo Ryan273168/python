ad905b16245a55487977e0a4f83eda9ea596f616
ryan273168 <ryan273168@gmail.com>
2022-04-30 15:33:34 +0800

    1234

---
+++ 20220430/20220430.py
@@ -0,0 +1,35 @@
+# import random
+# print(random.randrange(1, 7))
+
+# import random
+# print(random.randint(1, 6))
+
+# X = int(input("請输入一个数字:"))
+# sum = 0
+# i = 0
+# while i <= X:
+#     sum += i
+#     i += 1
+#     print(sum)
+
+# i = 1
+# while i < 6:
+#     if i == 3:
+#         break
+#     print(i)
+#     i += 1
+# else:
+#     print("正常結束")
+
+while True:
+    num = (input("請输入果汁編號:"))
+    if num == "1":
+        print("你的商品是:蘋果汁")
+    elif num == "2":
+        print("你的商品是:柳橙汁")
+    elif num == "3":
+        print("你的商品是:葡萄汁")
+    elif num == "4":
+        break
+    else:
+        print("!!!輸入錯誤查無此果汁!!!")
