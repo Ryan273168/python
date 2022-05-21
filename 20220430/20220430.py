c6bd6a2ca6ae6aae2ac3ef0c3f82cdb0939cf864
ryan273168 <ryan273168@gmail.com>
2022-05-21 13:31:29 +0800

    1234

--- 20220430/20220430.py
+++ 20220430/20220430.py
@@ -47,24 +47,24 @@
 
 # turtle.done()
 
-import turtle
-import time
+# import turtle
+# import time
 
-turtle.pendown()
-turtle.speed(0)
+# turtle.pendown()
+# turtle.speed(0)
 
-for i in range(60):
-    for j in range(12):
-        turtle.penup()
-        turtle.right(j * 30)
-        turtle.forward(100)
-        turtle.stamp()
-        turtle.home()
-    turtle.pendown()
-    turtle.right(i * 6)
-    turtle.forward(100)
-    turtle.home()
-    time.sleep(1)
-    turtle.clear()
+# for i in range(60):
+#     for j in range(12):
+#         turtle.penup()
+#         turtle.right(j * 30)
+#         turtle.forward(100)
+#         turtle.stamp()
+#         turtle.home()
+#     turtle.pendown()
+#     turtle.right(i * 6)
+#     turtle.forward(100)
+#     turtle.home()
+#     time.sleep(1)
+#     turtle.clear()
 
-turtle.done()
+# turtle.done()
--- 20220514/tempCodeRunnerFile.py
+++ 20220514/tempCodeRunnerFile.py
@@ -1,4 +1,14 @@
-i = ["a", "b", "c"]
-print(i)
-i[0] = 'A'
-print(i)
+i = []
+while True:
+    print("1.想加入菜單的果汁2.顯示出目前所有果汁3.離開系統")
+    num = (input("請输入功能選項:"))
+    if num == "1":
+        menu = (input("請輸入想加入菜單的果汁"))
+        if menu == menu:
+            print("此果汁已列入清單中")
+        else:
+            i.append(menu)
+    elif num == "2":
+        print(i)
+    elif num == "3":
+        break
